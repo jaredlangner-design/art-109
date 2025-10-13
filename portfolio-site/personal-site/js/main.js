e1982ea46ebcccf49fc08b69034c4a21f2b3ecbb
jaredlangner-design <jared.langner@sjsu.edu>
2025-10-13 16:10:31 -0700

    More updates to my portfolio

---
+++ portfolio-site/personal-site/js/main.js
@@ -0,0 +1,10 @@
+console.log("Yo what's up?");
+
+document.addEventListener('DOMContentLoaded', () => {
+            const menuToggle = document.querySelector('.menu-toggle');
+            const navLinks = document.querySelector('.nav-links');
+
+            menuToggle.addEventListener('click', () => {
+                navLinks.classList.toggle('active');
+            });
+        });
