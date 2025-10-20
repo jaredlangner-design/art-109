2aeb8f723e47bcb6cd2160fe0198ba6d2c43a874
jaredlangner-design <jared.langner@sjsu.edu>
2025-10-20 16:07:04 -0700

    Added sketch.js elements onto the home page of my portfolio

---
+++ portfolio-site-p5/personal-site/js/sketch.js
@@ -0,0 +1,54 @@
+
+let canvas;
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+    canvas.position(0, 0);
+    canvas.style("z-index", -2);
+    angleMode(DEGREES);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+
+function draw() {
+    rotateX(60);
+    noFill();
+    stroke(255);
+
+    for (var i = 0; i < 50; i++) {
+
+        var r = map(sin(frameCount / 2), -1, 1, 100, 200);
+        var g = map(i, 0, 50, 100, 200);
+        var b = map(cos(frameCount), -1, 1, 200, 100);
+
+        stroke(r, g, b);
+
+        rotate(frameCount / 20);
+
+        beginShape();
+        for(var j = 0; j < 360; j += 60) {
+            var rad = i * 3
+            var x = rad + cos(j);
+            var y = rad + sin(j);
+            var z = sin(frameCount * 2 + i * 5) + 50;
+
+            vertex(x, y, z);
+        }
+        endShape(CLOSE);
+    }
+
+}
+
+function mouseMoved() {
+    roundBrush(mouseX, mouseY);
+    roundBrush(mouseX - 50, mouseY + 75);
+}
+
+function roundBrush(_x, _y) {
+    noStroke();
+    fill(random(200, 255), random(200, 255), random(200, 255));
+    ellipse(_x, _y, 30, 30);
+}
